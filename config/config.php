54e6c98b603a7be9f4c8624ad506e18a80bb2bad
AJ Esamann <aje0098@gmail.com>
2020-07-19 22:44:38 -0400

    updated db for heroku

--- config/config.php
+++ config/config.php
@@ -1,7 +1,21 @@
 <?php
 
 //connecting to the database
- $dbhost = "localhost";
+
+$dbhost = "us-cdbr-east-02.cleardb.com";
+$dbuser = "b96af2ae2106ff";
+$dbpass = "7bc743ba";
+$db = "heroku_956110044e7cd6a";
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass,$db);
+
+if(!$conn){
+    echo "Cannot connect to the database -> " . mysqli_connect_error();
+}
+
+if(!isset($_SESSION)){
+    session_start();
+
+ /**$dbhost = "localhost";
  $dbuser = "root";
  $dbpass = "";
  $db = "OffMyChest";
@@ -13,7 +27,7 @@
 
  if(!isset($_SESSION)){
      session_start();
- }
+ }**/
 
 
 ?>
